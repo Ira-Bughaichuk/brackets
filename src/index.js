12b6f50c28e04f3d93a4234a9a4a38a5ab44748a
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-08-18 16:22:10 +0300

    feat: create func check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+    const openBrackets = [];
+    const closeBrackets = [];
+    const specialBrackets = new Set();
+    const matchingPairs = {};
+
+    for (const [open, close] of bracketsConfig) {
+        openBrackets.push(open);
+        closeBrackets.push(close);
+        matchingPairs[close] = open;
+
+        if (open === close) {
+            specialBrackets.add(open);
+        }
+    }
+
+    for (const char of str) {
+        if (openBrackets.includes(char)) {
+            if (specialBrackets.has(char) && stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else if (closeBrackets.includes(char)) {
+            const lastOpen = stack.pop();
+            if (lastOpen !== matchingPairs[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
